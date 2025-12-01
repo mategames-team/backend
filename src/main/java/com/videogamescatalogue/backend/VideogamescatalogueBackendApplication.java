5cc0b3af52dea9d1d97c763ff2cc68ad867a79f0
Yuliia <innishastudy@gmail.com>
2025-12-01 18:17:27 +0200

    first commit

---
+++ src/main/java/com/videogamescatalogue/backend/VideogamescatalogueBackendApplication.java
@@ -0,0 +1,13 @@
+package com.videogamescatalogue.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogamescatalogueBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideogamescatalogueBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/videogamescatalogue/backend/VideogamescatalogueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videogamescatalogue.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogamescatalogueBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
