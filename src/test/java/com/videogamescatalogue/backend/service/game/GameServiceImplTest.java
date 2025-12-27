3d6a86c0cfc813194b2a763262c1b9becb9e91e1
Yuliia <innishastudy@gmail.com>
2025-12-27 16:14:32 +0200

    created service tests

---
+++ src/test/java/com/videogamescatalogue/backend/service/game/GameServiceImplTest.java
@@ -0,0 +1,271 @@
+package com.videogamescatalogue.backend.service.game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import com.videogamescatalogue.backend.dto.external.ApiPlatformWrapper;
+import com.videogamescatalogue.backend.dto.external.ApiResponseFullGameDto;
+import com.videogamescatalogue.backend.dto.external.ApiResponseGameDto;
+import com.videogamescatalogue.backend.dto.external.ApiResponseGenreDto;
+import com.videogamescatalogue.backend.dto.external.ApiResponsePlatformDto;
+import com.videogamescatalogue.backend.dto.internal.game.GameDto;
+import com.videogamescatalogue.backend.dto.internal.game.GameWithStatusDto;
+import com.videogamescatalogue.backend.dto.internal.genre.GenreDto;
+import com.videogamescatalogue.backend.dto.internal.platform.PlatformDto;
+import com.videogamescatalogue.backend.mapper.game.GameMapper;
+import com.videogamescatalogue.backend.model.Game;
+import com.videogamescatalogue.backend.model.Genre;
+import com.videogamescatalogue.backend.model.Platform;
+import com.videogamescatalogue.backend.repository.GameRepository;
+import com.videogamescatalogue.backend.service.RawgApiClient;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+@ExtendWith(MockitoExtension.class)
+class GameServiceImplTest {
+    @Mock
+    private RawgApiClient apiClient;
+    @Mock
+    private GameMapper gameMapper;
+    @Mock
+    private GameRepository gameRepository;
+    @InjectMocks
+    private GameServiceImpl gameService;
+    private List<ApiResponseGameDto> apiResponse;
+    private ApiResponseGameDto apiResponseGameDto;
+    private List<Game> gameModelList;
+    private List<Long> responseGamesIds;
+    private List<Game> zeroGamesToSave;
+    private Pageable pageable;
+    private List<Game> oneGameToSave;
+    private Page<Game> oneGamePage;
+    private Game gameModel;
+    private GameDto gameDto;
+    private Page<GameDto> oneGameDtoPage;
+    private GameWithStatusDto gameWithDescrAndNullStatusDto;
+    private ApiResponseFullGameDto apiResponseFullGameDto;
+    private Game gameModelWithDescription;
+
+    @BeforeEach
+    void setUp() {
+        ApiPlatformWrapper apiPlatformWrapper = new ApiPlatformWrapper(
+                new ApiResponsePlatformDto("PC")
+        );
+        ApiResponseGenreDto apiResponseGenreDto = new ApiResponseGenreDto("Action");
+        apiResponseGameDto = new ApiResponseGameDto(
+                1L, "Game",
+                "2025-12-12", "link",
+                List.of(apiPlatformWrapper),
+                List.of(apiResponseGenreDto),
+                BigDecimal.valueOf(4.75));
+        apiResponse = List.of(apiResponseGameDto);
+
+        Platform platformModel = new Platform();
+        platformModel.setId(10L);
+        platformModel.setGeneralName(Platform.GeneralName.PC);
+        Genre genreModel = new Genre();
+        genreModel.setId(20L);
+        genreModel.setName(Genre.Name.ACTION);
+
+        gameModel = new Game();
+        gameModel.setApiId(1L);
+        gameModel.setName("Game");
+        gameModel.setYear(2025);
+        gameModel.setBackgroundImage("link");
+        gameModel.setPlatforms(Set.of(platformModel));
+        gameModel.setGenres(Set.of(genreModel));
+        gameModel.setApiRating(BigDecimal.valueOf(4.75));
+        gameModel.setDescription(null);
+        gameModelList = List.of(gameModel);
+        responseGamesIds = List.of(1L);
+        zeroGamesToSave = List.of();
+        pageable = PageRequest.of(0, 30);
+        oneGameToSave = List.of(gameModel);
+        oneGamePage = new PageImpl<>(gameModelList);
+
+        PlatformDto platformDto = new PlatformDto("PC");
+        GenreDto genreDto = new GenreDto("Action");
+        gameDto = new GameDto(
+                1L, "Game", 2025, "link",
+                Set.of(platformDto), Set.of(genreDto),
+                BigDecimal.valueOf(4.75), null
+        );
+        oneGameDtoPage = new PageImpl<>(List.of(gameDto));
+        gameWithDescrAndNullStatusDto = new GameWithStatusDto(
+                1L, "Game", 2025, "link",
+                Set.of(platformDto), Set.of(genreDto),
+                BigDecimal.valueOf(4.75),
+                "description", null
+        );
+
+        apiResponseFullGameDto = new ApiResponseFullGameDto(
+                1L, "Game", "description",
+                "2025-12-12", "link",
+                List.of(apiPlatformWrapper),
+                List.of(apiResponseGenreDto),
+                BigDecimal.valueOf(4.75)
+        );
+
+        gameModelWithDescription = new Game();
+        gameModelWithDescription.setApiId(1L);
+        gameModelWithDescription.setName("Game");
+        gameModelWithDescription.setYear(2025);
+        gameModelWithDescription.setBackgroundImage("link");
+        gameModelWithDescription.setPlatforms(Set.of(platformModel));
+        gameModelWithDescription.setGenres(Set.of(genreModel));
+        gameModelWithDescription.setApiRating(BigDecimal.valueOf(4.75));
+        gameModelWithDescription.setDescription("description");
+    }
+
+    @Test
+    void fetchBestGames_NoNewGames_SavesZeroGames() {
+        when(apiClient.getBestGames())
+                .thenReturn(apiResponse);
+        when(gameMapper.toModelList(apiResponse))
+                .thenReturn(gameModelList);
+        when(gameRepository.findAllByApiIdIn(responseGamesIds))
+                .thenReturn(gameModelList);
+        when(gameRepository.saveAll(zeroGamesToSave))
+                .thenReturn(null);
+
+        gameService.fetchBestGames();
+
+        verify(apiClient).getBestGames();
+        verify(gameMapper).toModelList(apiResponse);
+        verify(gameRepository).findAllByApiIdIn(responseGamesIds);
+        verify(gameRepository).saveAll(zeroGamesToSave);
+        verifyNoMoreInteractions(
+                apiClient, gameMapper, gameRepository
+        );
+    }
+
+    @Test
+    void fetchAndUpdateBestGames_validRequest_SavesAndUpdates() {
+        when(apiClient.getBestGames())
+                .thenReturn(apiResponse);
+        when(gameMapper.toModelList(apiResponse))
+                .thenReturn(gameModelList);
+        when(gameRepository.findAllByApiIdIn(responseGamesIds))
+                .thenReturn(gameModelList);
+        when(gameRepository.saveAll(oneGameToSave))
+                .thenReturn(null);
+
+        gameService.fetchAndUpdateBestGames();
+
+        verify(apiClient).getBestGames();
+        verify(gameMapper).toModelList(apiResponse);
+        verify(gameRepository).findAllByApiIdIn(responseGamesIds);
+        verify(gameRepository).saveAll(oneGameToSave);
+        verifyNoMoreInteractions(apiClient, gameMapper, gameRepository);
+    }
+
+    @Test
+    void getAllGamesFromDb_validRequest_returnsPageGameDtos() {
+        when(gameRepository.findAll(pageable))
+                .thenReturn(oneGamePage);
+        when(gameMapper.toDto(gameModel)).thenReturn(gameDto);
+
+        Page<GameDto> actual = gameService.getAllGamesFromDb(pageable);
+
+        assertNotNull(actual);
+        assertEquals(oneGameDtoPage, actual);
+
+        verify(gameRepository).findAll(pageable);
+        verify(gameMapper).toDto(gameModel);
+        verifyNoMoreInteractions(gameMapper, gameRepository);
+    }
+
+    @Test
+    void getByApiId_gameWithDescrInDbNoLoggedUser_returnsGameWithStatusDto() {
+        Long apiId = 1L;
+        gameModel.setDescription("description");
+
+        when(gameRepository.findByApiId(apiId))
+                .thenReturn(Optional.of(gameModel));
+        when(gameMapper.toDtoWithStatus(gameModel, null))
+                .thenReturn(gameWithDescrAndNullStatusDto);
+
+        GameWithStatusDto actual = gameService.getByApiId(apiId, null);
+
+        assertNotNull(actual);
+        assertEquals(gameWithDescrAndNullStatusDto, actual);
+        assertNull(actual.status());
+
+        verify(gameRepository).findByApiId(apiId);
+        verify(gameMapper).toDtoWithStatus(gameModel, null);
+        verifyNoMoreInteractions(gameMapper, gameRepository);
+        verifyNoInteractions(apiClient);
+    }
+
+    @Test
+    void getByApiId_gameWithoutDescrInDbNoLoggedUser_returnsGameWithStatusDto() {
+        Long apiId = 1L;
+
+        when(gameRepository.findByApiId(apiId))
+                .thenReturn(Optional.of(gameModel));
+        when(apiClient.getGameById(apiId))
+                .thenReturn(apiResponseFullGameDto);
+        when(gameRepository.save(gameModel))
+                .thenReturn(gameModel);
+
+        when(gameMapper.toDtoWithStatus(gameModel, null))
+                .thenReturn(gameWithDescrAndNullStatusDto);
+
+        GameWithStatusDto actual = gameService.getByApiId(apiId, null);
+
+        assertNotNull(actual);
+        assertEquals(gameWithDescrAndNullStatusDto, actual);
+        assertNull(actual.status());
+
+        verify(gameRepository).findByApiId(apiId);
+        verify(gameMapper).toDtoWithStatus(gameModel, null);
+        verify(apiClient).getGameById(apiId);
+        verifyNoMoreInteractions(gameMapper, gameRepository, apiClient);
+    }
+
+    @Test
+    void getAllGamesFromApi_validRequest_returnPageGameDto() {
+        when(apiClient.getAllGames(pageable))
+                .thenReturn(new PageImpl<>(apiResponse));
+        when(gameMapper.toModel(apiResponseGameDto))
+                .thenReturn(gameModel);
+        when(gameMapper.toDto(gameModel)).thenReturn(gameDto);
+
+        Page<GameDto> actual = gameService.getAllGamesFromApi(pageable);
+
+        assertNotNull(actual);
+        assertEquals(new PageImpl<>(
+                List.of(gameDto)), actual
+        );
+
+        verify(apiClient).getAllGames(pageable);
+        verifyNoInteractions(gameRepository);
+    }
+
+    @Test
+    void search_validRequest_returnPageGameDto() {
+        verifyNoInteractions(apiClient);
+    }
+
+    @Test
+    void apiSearch() {
+        verifyNoInteractions(gameRepository);
+    }
+}
---
+++ src/test/java/com/videogamescatalogue/backend/service/user/UserServiceImplTest.java
@@ -0,0 +1,261 @@
+package com.videogamescatalogue.backend.service.user;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import com.videogamescatalogue.backend.dto.internal.user.ChangePasswordRequestDto;
+import com.videogamescatalogue.backend.dto.internal.user.UpdateUserRequestDto;
+import com.videogamescatalogue.backend.dto.internal.user.UserRegistrationRequestDto;
+import com.videogamescatalogue.backend.dto.internal.user.UserResponseDto;
+import com.videogamescatalogue.backend.exception.InvalidInputException;
+import com.videogamescatalogue.backend.exception.RegistrationException;
+import com.videogamescatalogue.backend.mapper.user.UserMapper;
+import com.videogamescatalogue.backend.model.Role;
+import com.videogamescatalogue.backend.model.User;
+import com.videogamescatalogue.backend.repository.UserRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private UserMapper userMapper;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @InjectMocks
+    private UserServiceImpl userService;
+    private String email = "test@gmail.com";
+    private UserRegistrationRequestDto registrationRequestDto;
+    private User user;
+    private UserResponseDto responseDtoUser;
+    private Role role;
+    private UserResponseDto responseDtoBob;
+    private User userBob;
+    private UpdateUserRequestDto updateUserRequestDto;
+    private ChangePasswordRequestDto changePasswordRequestDto;
+    private ChangePasswordRequestDto changePasswordRequestDtoNoMatch;
+
+    @BeforeEach
+    void setUp() {
+        registrationRequestDto = new UserRegistrationRequestDto(
+                "profileName", "password5454764",
+                "password5454764", "test@gmail.com",
+                "about", "location"
+        );
+        role = new Role();
+        role.setId(1L);
+        role.setRole(Role.RoleName.ROLE_USER);
+
+        user = new User();
+        user.setId(10L);
+        user.setProfileName("profileName");
+        user.setPassword(
+                "$2a$10$iHLvIfNAFWubZLSUjY4kCeB9.gMurj6ePKehoCcHnn0dWEZBvnY9i"
+        );
+        user.setEmail("test@gmail.com");
+        user.setAbout("about");
+        user.setLocation("location");
+        user.getRoles().add(role);
+
+        responseDtoUser = new UserResponseDto(
+                10L, "profileName",
+                "about", "location"
+        );
+
+        userBob = new User();
+        userBob.setId(9L);
+        userBob.setProfileName("profileNameBob");
+        userBob.setPassword("hfgrhgthehethetheheheihd");
+        userBob.setEmail("testBob@gmail.com");
+        userBob.setAbout("aboutBob");
+        userBob.setLocation("locationBob");
+        userBob.getRoles().add(role);
+
+        responseDtoBob = new UserResponseDto(
+                9L, "profileNameBob",
+                "aboutBob", "locationBob"
+        );
+
+        updateUserRequestDto = new UpdateUserRequestDto(
+                "updated profileName", null,
+                null, null
+        );
+
+        changePasswordRequestDto = new ChangePasswordRequestDto(
+                "jfhgf747837",
+                "newpassword36445",
+                "newpassword36445"
+        );
+        changePasswordRequestDtoNoMatch = new ChangePasswordRequestDto(
+                "password",
+                "newpassword36445",
+                "newpassword364"
+        );
+    }
+
+    @Test
+    void registerUser_userExists_throwException() {
+        when(userRepository.existsByEmail(email))
+                .thenReturn(true);
+        assertThrows(RegistrationException.class,
+                () -> userService.registerUser(
+                        registrationRequestDto
+                ));
+
+    }
+
+    @Test
+    void registerUser_validRequest_returnUserDto() {
+        String encodedPassword = user.getPassword();
+
+        when(userRepository.existsByEmail(email))
+                .thenReturn(false);
+        when(userMapper.toModel(registrationRequestDto))
+                .thenReturn(user);
+        when(passwordEncoder.encode(
+                registrationRequestDto.password()))
+                .thenReturn(encodedPassword);
+        when(userRepository.save(user))
+                .thenReturn(user);
+        when(userMapper.toDto(user)).thenReturn(responseDtoBob);
+
+        UserResponseDto actual = userService.registerUser(
+                registrationRequestDto
+        );
+
+        assertEquals(responseDtoBob, actual);
+
+        verify(passwordEncoder).encode(registrationRequestDto.password());
+        verify(userRepository).existsByEmail(email);
+    }
+
+    @Test
+    void getUserInfo_usersMatch_returnAuthenticatedUserInfo() {
+        when(userMapper.toDto(user))
+                .thenReturn(responseDtoBob);
+
+        UserResponseDto actual = userService.getUserInfo(10L, user);
+
+        assertEquals(responseDtoBob, actual);
+
+        verifyNoInteractions(userRepository);
+    }
+
+    @Test
+    void getUserInfo_usersDoNotMatch_returnOtherUserInfo() {
+        when(userRepository.findById(9L))
+                .thenReturn(Optional.of(userBob));
+        when(userMapper.toDto(userBob))
+                .thenReturn(responseDtoBob);
+
+        UserResponseDto actual = userService.getUserInfo(9L, user);
+
+        assertEquals(responseDtoBob, actual);
+        assertNotEquals(responseDtoUser, actual);
+
+        verify(userRepository).findById(9L);
+        verify(userMapper).toDto(userBob);
+    }
+
+    @Test
+    void updateUserInfo_validRequest_returnUserResponseDto() {
+        when(userMapper.updateProfileInfo(
+                user, updateUserRequestDto
+        ))
+                .thenReturn(user);
+        when(userRepository.save(user))
+                .thenReturn(user);
+        when(userMapper.toDto(user)).thenReturn(responseDtoBob);
+
+        UserResponseDto actual = userService.updateUserInfo(
+                updateUserRequestDto, user
+        );
+
+        assertEquals(responseDtoBob, actual);
+
+        verify(userRepository).save(user);
+    }
+
+    @Test
+    void changePassword_currentPasswordInvalid_throwException() {
+        when(passwordEncoder.matches(
+                changePasswordRequestDto.currentPassword(),
+                user.getPassword()
+        )).thenReturn(false);
+        assertThrows(InvalidInputException.class,
+                () -> userService.changePassword(
+                        changePasswordRequestDto, user
+                ));
+
+        verify(passwordEncoder).matches(
+                changePasswordRequestDto.currentPassword(),
+                user.getPassword()
+        );
+        verifyNoMoreInteractions(passwordEncoder);
+        verifyNoInteractions(userRepository, userMapper);
+    }
+
+    @Test
+    void changePassword_passwordsDoNotMatch_throwException() {
+        when(passwordEncoder.matches(
+                changePasswordRequestDtoNoMatch.currentPassword(),
+                user.getPassword()
+        )).thenReturn(true);
+        assertThrows(InvalidInputException.class,
+                () -> userService.changePassword(
+                        changePasswordRequestDtoNoMatch, user
+                ));
+
+        verify(passwordEncoder).matches(
+                changePasswordRequestDtoNoMatch.currentPassword(),
+                user.getPassword()
+        );
+        verifyNoMoreInteractions(passwordEncoder);
+        verifyNoInteractions(userRepository, userMapper);
+    }
+
+    @Test
+    void changePassword_validRequest_returnUserResponseDto() {
+        String oldPassword = user.getPassword();
+        when(passwordEncoder.matches(
+                changePasswordRequestDto.currentPassword(),
+                oldPassword
+        )).thenReturn(true);
+        when(passwordEncoder.encode(
+                changePasswordRequestDto.newPassword()
+        ))
+                .thenReturn("hfgahfghrgrlgnlrg/lng/lahdhtoi");
+        when(userRepository.save(user)).thenReturn(user);
+        when(userMapper.toDto(user)).thenReturn(responseDtoBob);
+
+        UserResponseDto actual = userService.changePassword(
+                changePasswordRequestDto, user
+        );
+
+        assertNotNull(actual);
+        assertEquals(responseDtoBob, actual);
+
+        verify(passwordEncoder).matches(
+                changePasswordRequestDto.currentPassword(),
+                oldPassword
+        );
+        verify(passwordEncoder).encode(
+                changePasswordRequestDto.newPassword()
+        );
+        verify(userRepository).save(user);
+    }
+}
