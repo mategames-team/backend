038a73f51cd52d5aab6ab8fc420f3f88ce9d8682
Yuliia <innishastudy@gmail.com>
2025-12-28 12:48:53 +0200

    configured cors

--- src/main/java/com/videogamescatalogue/backend/config/CorsConfig.java
+++ src/main/java/com/videogamescatalogue/backend/config/CorsConfig.java
@@ -15,8 +15,9 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("Authorization", "Content-Type")
+                        .allowCredentials(false);
             }
         };
     }
