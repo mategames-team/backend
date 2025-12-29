e674437caf511665315172dfc5d7c10a719c22e0
Yuliia Tatarchuk <153382678+YuliiaNisha@users.noreply.github.com>
2025-12-29 19:00:07 +0200

    added response code (#33)

--- src/main/java/com/videogamescatalogue/backend/controller/UserGameController.java
+++ src/main/java/com/videogamescatalogue/backend/controller/UserGameController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "User Games", description = "Manage games in the authenticated user's library")
@@ -91,6 +93,7 @@ public class UserGameController {
             content = @Content
     )
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(
             @PathVariable Long id,
             @AuthenticationPrincipal User user
