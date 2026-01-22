93715e93ccb97420f1ab0db8a31870a1260d3713
Yuliia Tatarchuk <153382678+YuliiaNisha@users.noreply.github.com>
2026-01-22 21:44:55 +0200

    return token after registartion (#57)

--- src/main/java/com/videogamescatalogue/backend/mapper/user/UserMapper.java
+++ src/main/java/com/videogamescatalogue/backend/mapper/user/UserMapper.java
@@ -3,6 +3,7 @@ package com.videogamescatalogue.backend.mapper.user;
 import com.videogamescatalogue.backend.config.MapperConfig;
 import com.videogamescatalogue.backend.dto.internal.user.UpdateUserRequestDto;
 import com.videogamescatalogue.backend.dto.internal.user.UserRegistrationRequestDto;
+import com.videogamescatalogue.backend.dto.internal.user.UserRegistrationResponseDto;
 import com.videogamescatalogue.backend.dto.internal.user.UserResponseDto;
 import com.videogamescatalogue.backend.model.User;
 import org.mapstruct.Mapper;
@@ -16,6 +17,9 @@ public interface UserMapper {
 
     @Mapping(source = "id", target = "userGames", qualifiedByName = "getStatusDtoList")
     UserResponseDto toDto(User user);
+
+    @Mapping(source = "token", target = "token")
+    UserRegistrationResponseDto toRegistrationResponseDto(User user, String token);
 
     User updateProfileInfo(@MappingTarget User user, UpdateUserRequestDto requestDto);
 }
